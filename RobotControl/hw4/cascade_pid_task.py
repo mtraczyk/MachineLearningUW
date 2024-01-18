52bd76cd2dbf69d3d6656c309cde06292e0bd552
mtraczyk <michaltraczyk943@gmail.com>
2024-01-18 11:00:28 +0100

    Trying to get the cascade design working.

--- RobotControl/hw4/cascade_pid_task.py
+++ RobotControl/hw4/cascade_pid_task.py
@@ -19,15 +19,33 @@ if __name__ == '__main__':
     # If you want the simulation to be displayed more slowly, decrease rendering_freq
     # Note that this DOES NOT change the timestep used to approximate the physics of the simulation!
     drone_simulator = DroneSimulator(
-        model, data, viewer, desired_altitude = desired_altitude,
-        altitude_sensor_freq = 0.01, wind_change_prob = 0.1, rendering_freq = 1
-        )
+        model, data, viewer, desired_altitude=desired_altitude,
+        altitude_sensor_freq=0.01, wind_change_prob=0.1, rendering_freq=1
+    )
 
     # TODO: Create necessary PID controllers using PID class
-    ...
+    pid_altitude = PID(
+        gain_prop=10., gain_int=1., gain_der=1.,
+        sensor_period=drone_simulator.altitude_sensor_period
+    )
+
+    MULTIPLIER = 100
+    pid_acceleration = PID(
+        gain_prop=3., gain_int=1., gain_der=0.5,
+        sensor_period=(drone_simulator.altitude_sensor_period / MULTIPLIER)
+    )
+
+    # data.sensor("body_linacc").data[2] is the acceleration in the z direction
 
     # Increase the number of iterations for a longer simulation
     for i in range(4000):
-        # TODO: Use the PID controllers in a cascade designe to control the drone
-        ...
-        drone_simulator.sim_step(0)
+        # TODO: Use the PID controllers in a cascade design to control the drone
+        # inner loop PID controller which controls thrust based on the acceleration
+        # outer loop PID controller which controls acceleration based on the altitude
+        desired_acceleration = pid_altitude.output_signal(desired_altitude, drone_simulator.measured_altitudes)
+        for _ in range(MULTIPLIER):
+            desired_thrust = pid_acceleration.output_signal(desired_acceleration + 9.81,
+                                                            [data.sensor("body_linacc").data[2]])
+            print(f"Desired thrust: {desired_thrust}")
+            print(f"Desired acceleration: {desired_acceleration}")
+            drone_simulator.sim_step(desired_thrust)
